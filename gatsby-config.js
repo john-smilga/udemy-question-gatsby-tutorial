4da221f6f0ecaa27e14c4c1ec6bd20df693e094f
John <johnsmilga@gmail.com>
2020-05-04 10:28:44 -0700

    images names changed

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,7 +24,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images/`,
+        path: `${__dirname}/src/Images/`,
       },
     },
     {
